9e40db65bf8b6cacf7a01c9cade24deeebb4f8d5
gabriel <gabosoft.ape@gmail.com>
2019-08-05 15:42:57 -0500

    updating test

--- wizard/project_report_wizard.py
+++ wizard/project_report_wizard.py
@@ -5,13 +5,15 @@ class ProjectReportButton(models.TransientModel):
     _name = 'wizard.project.report'
 
     history_select = fields.Many2one('historial.clinico', string="Historial" , required=True)
-    
+
     @api.multi
     def print_project_report_pdf(self):
         data = {
             'ids': self.ids,
             'model': self._name,
             'record': self.history_select,
+            'name': self.history_select.nombre,
+            'partner_id': self.history_select.partner_id,
         }
         return self.env.ref('ofimatica_clinical_management.report_project_pdf').report_action(self, data=data)
 
