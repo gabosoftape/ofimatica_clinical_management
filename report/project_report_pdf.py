cbfe5b536784f65e83a747112858b7e7a3584984
gabriel <gabosoft.ape@gmail.com>
2019-08-05 14:16:55 -0500

    update report

--- report/project_report_pdf.py
+++ report/project_report_pdf.py
@@ -27,22 +27,14 @@ from odoo import models, api
 class ProjectReportParser(models.AbstractModel):
     _name = 'report.project_report_pdf.project_report_template'
 
+    @api.multi
     def get_report_values(self, docids, data=None):
-        name = data['record']
-        wizard_record = request.env['wizard.project.report'].search([])[-1] 
-        partner_id = self.env['res_partner'].search([('partner_id','=',self.wizard_record.partner_id)])       
-        vals = []
-        vals.append({
-                    'name': self.wizard_record.nombre,
-                    'id_document': self.partner_id.id_document,
-                    'function': self.partner_id.function,
-                })
-
-        return {
-            'vals': vals,
-            'manager': self.name,
-            'date_end': self.wizard_record.fecha,
-        }
+      docs = self.env['historial.clinico'].browse(docids)
+      return {
+          'doc_ids': docs.ids,
+          'doc_model': 'historial.clinico',
+          'docs': docs,
+      }
 
 
 
