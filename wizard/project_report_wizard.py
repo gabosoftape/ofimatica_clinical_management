a773b6775011ae77f346625f7ab64a4fbac90030
gabriel <gabosoft.ape@gmail.com>
2019-08-12 09:31:32 -0500

    stable version ofimatica
    Se dejaron estables los datos del backend

--- wizard/project_report_wizard.py
+++ wizard/project_report_wizard.py
@@ -51,25 +51,47 @@ class ProjectReportButton(models.TransientModel):
     def print_formula_report_pdf(self):
         nombre = self.history_select.nombre
         caption = self.env['res.partner'].search([('name', 'like', nombre)])
+        hs = self.history_select
         data = {
-            'record': self.history_select,
+            'record': hs,
             'nombre': caption.name,
             'documento': caption.id_document,
             'function': caption.function,
-            'fecha': self.history_select.fecha,
-            'rx_od': self.history_select.rx_od,
-            'rx_oi': self.history_select.rx_oi,
-            'rx_od_av': self.history_select.rx_od_av,
-            'rx_oi_av': self.history_select.rx_oi_av,
-            'rx_add': self.history_select.rx_add,
-            'diagnostico_primario': self.history_select.diagnostico,
+            'fecha': hs.fecha,
+            'diagnostico_primario': hs.diagnostico,
             'oftalmoscopia': 'sin datos aun',
-            'conducta': self.history_select.plan,
-            'is_remision': self.history_select.is_remision,
+            'conducta': hs.plan,
+            'is_remision': hs.is_remision,
+            'rx_final_od_esf': hs.rx_final_od_esf,
+            'rx_final_od_cil': hs.rx_final_od_cil,
+            'rx_final_od_eje': hs.rx_final_od_eje,
+            'rx_final_od_add': hs.rx_final_od_add,
+            'rx_final_oi_esf': hs.rx_final_oi_esf,
+            'rx_final_oi_cil': hs.rx_final_oi_cil,
+            'rx_final_oi_eje': hs.rx_final_od_eje,
+            'rx_final_oi_add': hs.rx_final_od_add,
+            'rx_final_od_dp': hs.rx_final_od_dp,
+            'rx_final_od_dnp': hs.rx_final_od_dnp,
+            'rx_final_oi_dp': hs.rx_final_oi_dp,
+            'rx_final_oi_dnp': hs.rx_final_oi_dnp,
+            'rx_final_od_vp_esf': hs.rx_final_od_vp_esf,
+            'rx_final_od_vp_cil': hs.rx_final_od_vp_cil,
+            'rx_final_oi_vp_esf': hs.rx_final_oi_vp_esf,
+            'rx_final_oi_vp_cil': hs.rx_final_oi_vp_cil,
+            'prismas': hs.prismas,
+            'tipo_de_lente': hs.tipo_de_lente,
+            'diagnostico': hs.diagnostico,
+            'plan': hs.plan,
+            'dx_primario': hs.dx_primario,
+            'dx_secundario': hs.dx_secundario,
+            'dx_terciario': hs.dx_terciario,
+            'pres_tipo_lente':hs.pres_tipo_lente,
+            'pres_material': hs.pres_material,
+            'pres_filtro': hs.pres_filtro,
         }
         # Datos
         print(data)
-        return self.env.ref('ofimatica_clinical_management.report_remission_pdf').report_action(self, data=data)
+        return self.env.ref('ofimatica_clinical_management.report_formula_pdf').report_action(self, data=data)
 
     @api.multi
     def print_project_report_xls(self):
