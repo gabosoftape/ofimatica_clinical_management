d0a765538f1932a2869826a31cd4b12a39bc7688
gabriel <gabosoft.ape@gmail.com>
2019-09-24 10:09:08 -0500

    update values back into sede defaults

--- models/res_company.py
+++ models/res_company.py
@@ -9,13 +9,13 @@ class clinicalCompany(models.Model):
     pbx_fax = fields.Char('PBX/FAX')
     rte_legal = fields.Char('Representante Legal')
     sede_principal = fields.Selection(string="Sede Principal", selection=[
-        ('1', 'CHIPICHAPE'),
-        ('2', 'UNICENTRO'),
-        ('3', 'TECNIOPTICA 2142'),
-        ('4', 'TECNIOPTICA 1090'),
-        ('5', 'TECNIOPTICA 1195'),
-        ('6', 'TECNIOPTICA 1005'),
-        ('7', 'PRINCIPAL SAS'),
-        ('8', 'COMERCIAL 1189'),
-        ('9', 'COMERCIAL 1187'),
+        ('CHIPICHAPE', 'CHIPICHAPE'),
+        ('UNICENTRO', 'UNICENTRO'),
+        ('TECNIOPTICA 2142', 'TECNIOPTICA 2142'),
+        ('TECNIOPTICA 1090', 'TECNIOPTICA 1090'),
+        ('TECNIOPTICA 1195', 'TECNIOPTICA 1195'),
+        ('TECNIOPTICA 1005', 'TECNIOPTICA 1005'),
+        ('PRINCIPAL SAS', 'PRINCIPAL SAS'),
+        ('COMERCIAL 1189', 'COMERCIAL 1189'),
+        ('COMERCIAL 1187', 'COMERCIAL 1187'),
     ])
